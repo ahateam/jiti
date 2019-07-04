8b8e034972d414263ff9c7f5fab87971a2b21584
Li-jiayi <791427173@qq.com>
2019-07-04 17:52:34 +0800

    新增初版打车  打听小功能

--- src/main/java/zyxhj/jiti/service/SortService.java
+++ src/main/java/zyxhj/jiti/service/SortService.java
@@ -7,12 +7,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.druid.pool.DruidPooledConnection;
+import com.alibaba.fastjson.JSONObject;
 import com.alicloud.openservices.tablestore.SyncClient;
 import com.alicloud.openservices.tablestore.model.Column;
 import com.alicloud.openservices.tablestore.model.PrimaryKey;
+import com.alicloud.openservices.tablestore.model.search.SearchQuery;
 
 import zyxhj.core.domain.Car;
+import zyxhj.core.domain.Inquire;
 import zyxhj.core.repository.CarRepository;
+import zyxhj.core.repository.InquireRepository;
 import zyxhj.jiti.domain.Advert;
 import zyxhj.jiti.repository.AdvertRepository;
 import zyxhj.utils.IDUtils;
@@ -20,6 +24,8 @@ import zyxhj.utils.Singleton;
 import zyxhj.utils.api.ServerException;
 import zyxhj.utils.data.ts.ColumnBuilder;
 import zyxhj.utils.data.ts.PrimaryKeyBuilder;
+import zyxhj.utils.data.ts.TSQL;
+import zyxhj.utils.data.ts.TSQL.OP;
 import zyxhj.utils.data.ts.TSRepository;
 
 public class SortService {
@@ -28,12 +34,14 @@ public class SortService {
 
 	private AdvertRepository advertRepository;
 	private CarRepository carRepository;
+	private InquireRepository inquireRepository;
 
 	public SortService() {
 		try {
 
 			advertRepository = Singleton.ins(AdvertRepository.class);
 			carRepository = Singleton.ins(CarRepository.class);
+			inquireRepository = Singleton.ins(InquireRepository.class);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
@@ -73,8 +81,8 @@ public class SortService {
 	}
 
 	// 创建打车信息
-	public Car createCar(SyncClient client, String title, String content, String province, String city, String region,
-			String pos, Byte type, String tags) throws Exception {
+	public Car createCar(SyncClient client, String title, String content, String province, String departure,
+			String destination, String city, String region, String pos, Byte type, String tags) throws Exception {
 		Car c = new Car();
 		long id = IDUtils.getSimpleId();
 		c._id = IDUtils.simpleId2Hex(id).substring(0, 4);
@@ -85,6 +93,8 @@ public class SortService {
 		c.province = province;
 		c.city = city;
 		c.region = region;
+		c.departure = departure;
+		c.destination = destination;
 		c.pos = pos;
 		c.type = (long) type;
 		c.time = new Date();
@@ -103,7 +113,8 @@ public class SortService {
 
 	// 修改打车信息
 	public void editCar(SyncClient client, Long id, Long carId, String title, String content, String province,
-			String city, String region, String pos, Byte type, String tags) throws Exception {
+			String departure, String destination, String city, String region, String pos, Byte type, String tags)
+			throws Exception {
 		PrimaryKey pk = new PrimaryKeyBuilder().add("_id", id).add("id", carId).build();
 		ColumnBuilder cb = new ColumnBuilder();
 		cb.add("title", title);
@@ -111,6 +122,8 @@ public class SortService {
 		cb.add("province", province);
 		cb.add("city", city);
 		cb.add("region", region);
+		cb.add("departure", departure);
+		cb.add("destination", destination);
 		cb.add("pos", pos);
 		cb.add("type", (long) type);
 		cb.add("tags", tags);
@@ -120,10 +133,68 @@ public class SortService {
 	}
 
 	// 获取打车信息 根据条件获取
-	public List<Car> getCars(SyncClient client,Byte status,Byte type) {
-		
-		
-		return null;
-
+	public JSONObject getCars(SyncClient client, Byte status, Byte type, String region, Integer count, Integer offset)
+			throws Exception {
+		TSQL ts = new TSQL();
+		ts.Term(OP.AND, "status", (long) status).Term(OP.AND, "type", (long) type);
+		if (region != null) {
+			ts.Term(OP.AND, "region", region);
+		}
+		ts.setLimit(count);
+		ts.setOffset(offset);
+		ts.setGetTotalCount(true);
+		SearchQuery query = ts.build();
+		return TSRepository.nativeSearch(client, carRepository.getTableName(), "CarInfoIndex", query);
 	}
+
+	// 根据输入的地址查询打车信息
+	public JSONObject getCarByAdress(SyncClient client, String departure, String destination, Integer count,
+			Integer offset) throws Exception {
+		TSQL ts = new TSQL();
+		ts.Term(OP.AND, "departure", departure).Term(OP.AND, "destination", destination);
+		ts.setLimit(count);
+		ts.setOffset(offset);
+		ts.setGetTotalCount(true);
+
+		SearchQuery query = ts.build();
+		return TSRepository.nativeSearch(client, carRepository.getTableName(), "CarInfoIndex", query);
+	}
+
+	// 创建打听
+	public Inquire createInquire(SyncClient client, String title, String content, String province, Byte type,
+			String city, String region, String tags) throws Exception {
+		Inquire in = new Inquire();
+		long id = IDUtils.getSimpleId();
+		in._id = IDUtils.simpleId2Hex(id).substring(0, 4);
+		in.id = id;
+
+		in.title = title;
+		in.content = content;
+		in.type = (long) type;
+		in.province = province;
+		in.city = city;
+		in.region = region;
+		in.tags = tags;
+		in.time = new Date();
+
+		inquireRepository.insert(client, in, false);
+		return in;
+	}
+
+	public JSONObject getInquire(SyncClient client, String region, Byte type, Integer count, Integer offset)
+			throws Exception {
+		TSQL ts = new TSQL();
+		ts.Term(OP.AND, "region", region).Term(OP.AND, "type", type);
+		ts.setLimit(count);
+		ts.setOffset(offset);
+		ts.setGetTotalCount(true);
+		SearchQuery query = ts.build();
+		return TSRepository.nativeSearch(client, inquireRepository.getTableName(), "InquireInfoIndex", query);
+	}
+
+	public void delInquire(SyncClient client, Long id, Long inquireId) throws Exception {
+		PrimaryKey pk = new PrimaryKeyBuilder().add("_id", id).add("id", inquireId).build();
+		TSRepository.nativeDel(client, inquireRepository.getTableName(), pk);
+	}
+
 }

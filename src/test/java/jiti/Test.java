059304e4c2fbd084b701f7bf8f5f9d3bd7aefa9c
jXians <zheng_xing_jiang@163.com>
2019-08-19 20:03:16 +0800

    修改Test文件

--- src/test/java/jiti/Test.java
+++ src/test/java/jiti/Test.java
@@ -1,9 +1,17 @@
 package jiti;
 
-import com.alibaba.druid.pool.DruidPooledConnection;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.alibaba.druid.pool.DruidPooledConnection;
+import com.alibaba.fastjson.JSONArray;
+
+import zyxhj.jiti.domain.ORGUser;
+import zyxhj.jiti.repository.ORGUserRepository;
 import zyxhj.jiti.service.ORGService;
+import zyxhj.utils.api.ServerException;
 import zyxhj.utils.data.DataSource;
+import zyxhj.utils.data.EXP;
 
 public class Test {
 
@@ -33,6 +41,52 @@ public class Test {
 		for (int i = 0; i < (j / 100) + 1; i++) {
 			System.out.println(i);
 		}
+	}
+	
+	
+	@org.junit.Test
+	public void testAAA() throws ServerException {
+		
+		int[] a = {101,102};
+
+		JSONArray js = (JSONArray) JSONArray.toJSON(a);
+		js.add(103);
+		System.out.println(js.toJSONString());
+		
+		EXP tag = testJsonContainsORKey(js, "roles", null);
+		
+		EXP tags = EXP.INS().key("org_id", 400942027079516L).and(tag);
+		
+		StringBuffer sb = new StringBuffer();
+		List<Object> params = new ArrayList<Object>();
+		tags.toSQL(sb, params);
+		System.out.println(sb.toString());
+		
+		ORGUserRepository user = new ORGUserRepository();
+		
+		List<ORGUser> userlist = user.getList(conn, tags, 10, 0);
+		
+		System.out.println(userlist.size());
+		
+	}
+	
+	public EXP testJsonContainsORKey(JSONArray tags, String column, String tagGroup) throws ServerException {
+		
+		if(tags==null && tags.size()==0) {
+			return null;
+		}
+		String path;
+		if(tagGroup == null) {
+			path = "$";
+		}else {
+			path = "$."+tagGroup;
+		}
+		EXP tag = EXP.INS();
+		for(int i = 0; i < tags.size(); i++) {
+			System.out.println(tags.get(i));
+			tag.or(EXP.JSON_CONTAINS(column, path, tags.get(i)));
+		}
+		return tag;
 	}
 
 }
